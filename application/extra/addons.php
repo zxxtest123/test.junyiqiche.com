d3f1adfea76ef6e8985c828b11b44881014a9aec
Cyruszx <17360268104@139.com>
2019-03-12 20:10:03 +0800

    改

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,6 +4,14 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'ems_send' => 
+    array (
+      0 => 'faems',
+    ),
+    'ems_notice' => 
+    array (
+      0 => 'faems',
+    ),
   ),
   'route' => 
   array (
@@ -11,5 +19,8 @@ return array (
     '/example/d/[:name]' => 'example/demo/index',
     '/example/d1/[:name]' => 'example/demo/demo1',
     '/example/d2/[:name]' => 'example/demo/demo2',
+    '/third$' => 'third/index/index',
+    '/third/connect/[:platform]' => 'third/index/connect',
+    '/third/callback/[:platform]' => 'third/index/callback',
   ),
 );
